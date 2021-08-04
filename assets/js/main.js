c9b99a80375a3931fd679bc4e75306309b32aa2b
shariar-hriad <76766167+shariar-hriad@users.noreply.github.com>
2021-08-04 12:17:31 +0600

    dfd

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,4 +18,24 @@ jQuery(document).ready(function ($) {
     delay: 10,
     time: 1000,
   });
+
+  // Team Carousel
+
+  $(".team_slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
 });
